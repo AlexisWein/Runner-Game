6dd778ebe28117727f754adbcf31b650d5e3e1c7
Alexis Weintraub <aweintraub7@fordham.edu>
2023-01-19 15:20:12 -0500

    added game over screen

--- runner.py
+++ runner.py
@@ -1,29 +1,106 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surf = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
+    score_rect = score_surf.get_rect(center = (400, 50))
+    screen.blit(score_surf, score_rect)
+    print(current_time)
+    return current_time
+
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+game_active = False
+start_time = 0
+score = 0
 
 # # create surface with a solid color
 # test_surface = pygame.Surface((100,200))
 # test_surface.fill('pink')
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
-text_surface = test_font.render('My Game', False, 'Black')                #second value is anti-aliasing (smoothing out the text)
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+
+
+# score_surf = test_font.render('My Game', False, (64, 64, 64)).convert()                #second value is anti-aliasing (smoothing out the text)
+# score_rect = score_surf.get_rect(center = (400, 50))
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()      #removes the white/black background
+snail_rect = snail_surface.get_rect(midbottom = (600, 300))
+#snail_x_pos = 600
+
+player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80, 300))                        #creates a rectangle to make positioning easier
+
+player_gravity = 0
+
+# Intro Screen
+player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()        #start by importing the image
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)                                #scale the image and overwrite the original
+player_stand_rect = player_stand.get_rect(center = (400, 200))                              #create rectangle
+
+game_name = test_font.render('Pixel Runner', False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center = (400, 80))
+
+game_message = test_font.render('Press space to run', False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center = (400, 340))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
-    screen.blit(sky_surface, (0,0))         #this is the order they will appear onscreen
-    screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface, (300, 50))
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                    player_gravity = -20
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
+                start_time = int(pygame.time.get_ticks() / 1000)
+
+    if game_active:
+        screen.blit(sky_surface, (0,0))         #this is the order they will appear onscreen
+        screen.blit(ground_surface, (0,300))
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        # screen.blit(score_surf, score_rect)
+        score = display_score()
+
+        # snail animation
+        snail_rect.left -= 4
+        if snail_rect.left < -100: snail_rect.left = 800
+        screen.blit(snail_surface, snail_rect)
+        # player_rect.left += 1                  #don't move the surface, move the rectangle that contains the surface
+
+        # place player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300: player_rect.bottom = 300
+        screen.blit(player_surface, player_rect)
+
+        # collision with snail
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill((94, 129, 162))
+        screen.blit(player_stand, player_stand_rect)
+
+        score_message = test_font.render(f'Your score: {score}', False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center = (400, 330))
+        screen.blit(game_name, game_name_rect)
+
+        if score == 0: screen.blit(game_message, game_message_rect)
+        else: screen.blit(score_message, score_message_rect)
+
 
     pygame.display.update()
     clock.tick(60)                          #maxiumum framerate
